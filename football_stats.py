27beccde6d243c6705b5af18b959c8f9fa22c66e
Victor-Pessoa <123887770+Victor-Pessoa@users.noreply.github.com>
2023-04-24 19:32:56 -0400

    Update football_stats.py

--- football_stats.py
+++ football_stats.py
@@ -1,2 +1,27 @@
-if __name__ == "__main__":
-    pass
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://www.cbssports.com/nfl/stats/'
+
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+
+touchdowns_link = soup.find('a', {'href': '/nfl/stats/playersort/nfl/year-2021-season-regular-category-touchdowns'}) # find the link to the touchdowns page
+touchdowns_url = 'https://www.cbssports.com' + touchdowns_link['href']
+
+response = requests.get(touchdowns_url)
+soup = BeautifulSoup(response.text, 'html.parser')
+
+table = soup.find('table', {'class': 'data'}) # find the table with class 'data'
+tbody = table.find('tbody') # find the table body
+rows = tbody.find_all('tr') # find all table rows
+
+print('Top 20 players with the highest number of touchdowns:\n')
+
+for row in rows[:20]:
+    data = row.find_all('td')
+    player = data[0].text.strip()
+    position = data[1].text.strip()
+    team = data[2].text.strip()
+    touchdowns = data[6].text.strip()
+    print(f'{player} ({position}, {team}): {touchdowns} touchdowns')
