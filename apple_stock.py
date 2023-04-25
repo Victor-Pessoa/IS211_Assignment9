cab4743d5696a563046f5106ca0ef97a7dc72a4a
Victor-Pessoa <123887770+Victor-Pessoa@users.noreply.github.com>
2023-04-24 20:04:53 -0400

    Update apple_stock.py

--- apple_stock.py
+++ apple_stock.py
@@ -1,2 +1,19 @@
-if __name__ == "__main__":
-    pass
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://finance.yahoo.com/quote/AAPL/history?p=AAPL'
+
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+
+table = soup.find('table', {'class': 'W(100%) M(0)'}) 
+tbody = table.find('tbody') 
+rows = tbody.find_all('tr') 
+
+print('Apple stock data:\n')
+
+for row in rows:
+    data = row.find_all('td')
+    date = data[0].text.strip()
+    close_price = data[4].text.strip()
+    print(f'{date}: {close_price}')
